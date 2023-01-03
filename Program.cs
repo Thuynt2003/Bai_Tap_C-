393c4aafde7368b689b4eb38d2fe1c3e263b9c12
Thuynt2003 <101488871+Thuynt2003@users.noreply.github.com>
2023-01-03 11:42:09 +0700

    session1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using T2204M.session1;
+
+public class program
+{
+    public static void Main(String[]args)
+    {
+        int x = 10;
+        double y = 3.14;
+        Console.WriteLine("Hello world:" + x);
+        String s = "T2204M";
+
+        int[] ns = new int[3];
+        ns[0] = 15;
+        ns[1] = 20;
+        ns[2] = 25;
+        foreach(int n in ns)
+        {
+            //n
+        }   
+        for (int i = 0;i< ns.Length; i++)
+        {
+            //ns[i] 
+            string input = Console.ReadLine();
+            int num = Convert.ToInt32(input);
+            Console.WriteLine("num =" + num);
+            Hument h = new Hument();
+            h.Run();
+        }    
+    }
+}
---
+++ session1/Hument.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2204M.session1
+{
+    public class Hument
+    {
+        public string name;
+        public int age;
+        public void Eat()
+        {
+            Console.WriteLine("Eating....");
+        }
+        public void Run()
+        {
+            Console.WriteLine(this.name + "running.....");
+
+        }
+    }
+}
